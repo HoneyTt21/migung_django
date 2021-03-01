076f502db899a6f467e50be28a450d74201e9fd4
Minjun Kim <honeytt21@kaist.ac.kr>
2021-03-02 08:57:35 +0900

    hide secret key

---
+++ users/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+
+class User(AbstractUser):
+
+    """ Custom User Model """
+
+    SECRET_LENGTH = 15
+
+    # null avoids crashing for existing objects to have null value
+    # blank avoids mandatory inputs for forms
+
+    superhost = models.BooleanField(default=False)
+    email_verified = models.BooleanField(default=False)
+    email_key = models.CharField(max_length=SECRET_LENGTH, default="", blank=True)
